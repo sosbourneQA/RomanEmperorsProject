f2188adaf86c99e4bc87a2a185717d178f8cafa5
luke <lukesosbourne@gmail.com>
2020-05-06 15:18:44 +0100

    created EmperorNotFoundException.

--- src/main/java/com/qa/exceptions/EmperorNotFoundException.java
+++ src/main/java/com/qa/exceptions/EmperorNotFoundException.java
@@ -1,4 +1,10 @@
 package com.qa.exceptions;
 
-public class EmperorNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Emperor does not exist")
+public class EmperorNotFoundException extends EntityNotFoundException {
 }
