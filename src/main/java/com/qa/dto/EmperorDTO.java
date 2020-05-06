de513bf16155bc3a541d37004dc3c7ce9c9a7a11
luke <lukesosbourne@gmail.com>
2020-05-06 15:30:10 +0100

    created Emperor dto and started service.

--- src/main/java/com/qa/dto/EmperorDTO.java
+++ src/main/java/com/qa/dto/EmperorDTO.java
@@ -1,4 +1,52 @@
 package com.qa.dto;
 
 public class EmperorDTO {
+
+    private Long id;
+    private String name;
+    private String reignStart;
+    private String reignEnd;
+
+
+    public EmperorDTO() {
+    }
+
+    public EmperorDTO(Long id, String name, String reignStart, String reignEnd) {
+        this.id = id;
+        this.name = name;
+        this.reignStart = reignStart;
+        this.reignEnd = reignEnd;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReignStart() {
+        return reignStart;
+    }
+
+    public void setReignStart(String reignStart) {
+        this.reignStart = reignStart;
+    }
+
+    public String getReignEnd() {
+        return reignEnd;
+    }
+
+    public void setReignEnd(String reignEnd) {
+        this.reignEnd = reignEnd;
+    }
 }
--- src/main/java/com/qa/service/EmperorService.java
+++ src/main/java/com/qa/service/EmperorService.java
@@ -1,4 +1,21 @@
 package com.qa.service;
 
+
+import com.qa.repo.EmperorsRespository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmperorService {
+
+    private final EmperorsRespository repo;
+    private final ModelMapper mapper;
+
+    @Autowired
+    public EmperorService(EmperorsRespository repo, ModelMapper mapper) {
+        this.repo = repo;
+        this.mapper = mapper;
+    }
+
 }
