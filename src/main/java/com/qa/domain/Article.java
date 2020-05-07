c4db4c3df4eaf45ecebe998237b0997d8b6dd715
luke <lukesosbourne@gmail.com>
2020-05-07 18:36:15 +0100

    found an issue in the Article domain.

---
+++ src/main/java/com/qa/domain/Article.java
@@ -0,0 +1,53 @@
+package com.qa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String text;
+
+    public Article() {
+    }
+
+    public Article(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return id.equals(article.id) &&
+                text.equals(article.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
+    }
+}
---
+++ src/main/java/com/qa/repo/ArticlesRepository.java
@@ -0,0 +1,12 @@
+package com.qa.repo;
+
+import com.qa.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticlesRepository extends JpaRepository<Article, Long> {
+
+
+}
+
