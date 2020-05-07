7e1272d49eef5a7309c5b35e18b36d544f0ec9b0
luke <lukesosbourne@gmail.com>
2020-05-07 15:14:29 +0100

    created historian not found exception.

--- src/main/java/com/qa/exceptions/HistorianNotFoundException.java
+++ src/main/java/com/qa/exceptions/HistorianNotFoundException.java
@@ -1,4 +1,10 @@
 package com.qa.exceptions;
 
-public class HistorianNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.persistence.EntityNotFoundException;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Emperor does not exist")
+public class HistorianNotFoundException extends EntityNotFoundException {
 }
