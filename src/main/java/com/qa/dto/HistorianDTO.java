eb1c4df8336e2c7729c5a5f87099f72fc09bff6c
luke <lukesosbourne@gmail.com>
2020-05-07 15:48:26 +0100

    degubbing.

--- src/main/java/com/qa/dto/HistorianDTO.java
+++ src/main/java/com/qa/dto/HistorianDTO.java
@@ -1,41 +1,41 @@
-package com.qa.dto;
-
-public class HistorianDTO {
-
-    private Long id;
-    private String name;
-    private String speciality;
-
-    public HistorianDTO() {
-    }
-
-    public HistorianDTO(Long id, String name, String speciality) {
-        this.id = id;
-        this.name = name;
-        this.speciality = speciality;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-}
+//package com.qa.dto;
+//
+//public class HistorianDTO {
+//
+//    private Long id;
+//    private String name;
+//    private String speciality;
+//
+//    public HistorianDTO() {
+//    }
+//
+//    public HistorianDTO(Long id, String name, String speciality) {
+//        this.id = id;
+//        this.name = name;
+//        this.speciality = speciality;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getSpeciality() {
+//        return speciality;
+//    }
+//
+//    public void setSpeciality(String speciality) {
+//        this.speciality = speciality;
+//    }
+//}
