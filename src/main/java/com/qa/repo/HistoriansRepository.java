80625bcba5b06144f2c9c3d57fe3bb691f1d295c
luke <lukesosbourne@gmail.com>
2020-05-07 15:15:52 +0100

    created historian repo.

--- src/main/java/com/qa/repo/HistoriansRepository.java
+++ src/main/java/com/qa/repo/HistoriansRepository.java
@@ -1,4 +1,12 @@
 package com.qa.repo;
 
-public interface HistoriansRepository {
+
+import com.qa.domain.Historian;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HistoriansRepository extends JpaRepository<Historian, Long> {
+
+
 }
