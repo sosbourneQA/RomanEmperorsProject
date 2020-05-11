3c62544f6709661e1c26d65a44fe6c699651ed19
luke <lukesosbourne@gmail.com>
2020-05-11 20:48:29 +0100

    managed to get the api calls to work for emperor entity.

---
+++ target/classes/static/article.js
@@ -0,0 +1,41 @@
+const REQ = new XMLHttpRequest();
+function createArticle() {
+    let data = `{"text": "this is text", "emp": {id: 1} }`;
+    REQ.open("POST", "http://localhost:8181/createArticle");
+    REQ.setRequestHeader("Content-Type", "Application/json");
+    REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
+    REQ.send(data);
+
+    REQ.onload = () => {
+        if (REQ.status === 201) {
+            console.log(REQ.response);
+            console.log("article created");
+        } else {
+            console.log("handle error");
+        }
+    }
+}
+let buttCreateArticle = document.querySelector("#three");
+buttCreateArticle.addEventListener("click", createArticle);
+
+
+
+
+const REQ2 = new XMLHttpRequest();
+function getAllArticles() {}
+
+
+const REQ4 = new XMLHttpRequest();
+function getArticleById() {}
+
+
+const REQ6 = new XMLHttpRequest();
+function updateArticle() {}
+
+
+const REQ8 =  new XMLHttpRequest();
+function deleteArticle() {}
+
+
+
+
