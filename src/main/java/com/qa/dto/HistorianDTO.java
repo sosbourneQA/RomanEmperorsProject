cb1eb54b93c723f8084afae5862e428b1870f95b
luke <lukesosbourne@gmail.com>
2020-05-14 12:02:38 +0100

    commented out the historian classes which could potentially be expanded upon once the MVP in completed

--- src/main/java/com/qa/dto/HistorianDTO.java
+++ src/main/java/com/qa/dto/HistorianDTO.java
@@ -1,58 +1,58 @@
-package com.qa.dto;
-
-import java.util.Objects;
-
-public class HistorianDTO {
-
-    private Long id;
-    private String name;
-    private String speciality;
-
-    public HistorianDTO() {
-    }
-
-    public HistorianDTO(Long id, String name, String speciality) {
-        this.id = id;
-        this.name = name;
-        this.speciality = speciality;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        HistorianDTO that = (HistorianDTO) o;
-        return id.equals(that.id) &&
-                name.equals(that.name) &&
-                speciality.equals(that.speciality);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, speciality);
-    }
-}
+//package com.qa.dto;
+//
+//import java.util.Objects;
+//
+//public class HistorianDTO {
+//
+//    private Long id;
+//    private String name;
+//    private String speciality;
+//
+//    public HistorianDTO() {
+//    }
+//
+//    public HistorianDTO(Long id, String name, String speciality) {
+//        this.id = id;
+//        this.name = name;
+//        this.speciality = speciality;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getSpeciality() {
+//        return speciality;
+//    }
+//
+//    public void setSpeciality(String speciality) {
+//        this.speciality = speciality;
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        HistorianDTO that = (HistorianDTO) o;
+//        return id.equals(that.id) &&
+//                name.equals(that.name) &&
+//                speciality.equals(that.speciality);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id, name, speciality);
+//    }
+//}
--- src/main/java/com/qa/exceptions/HistorianNotFoundException.java
+++ src/main/java/com/qa/exceptions/HistorianNotFoundException.java
@@ -1,10 +1,10 @@
-package com.qa.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import javax.persistence.EntityNotFoundException;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Historian does not exist")
-public class HistorianNotFoundException extends EntityNotFoundException {
-}
+//package com.qa.exceptions;
+//
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ResponseStatus;
+//
+//import javax.persistence.EntityNotFoundException;
+//
+//@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "The Historian does not exist")
+//public class HistorianNotFoundException extends EntityNotFoundException {
+//}
